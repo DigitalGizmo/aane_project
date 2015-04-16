22559a086ae9758780bc23a3ac5d1a7646a709cd
Don Button <donpublic@digitalgizmo.com>
2015-04-16 10:06:09 -0400

    change grant script

---
+++ scripts/grant_aanedata_user.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+dbname="aanedata"
+username="root"
+
+psql $dbname $username << EOF
+	GRANT ALL ON ALL TABLES IN SCHEMA public to 'aanedata_user';
+	GRANT ALL ON ALL SEQUENCES IN SCHEMA public to 'aanedata_user';
+	GRANT ALL ON ALL FUNCTIONS IN SCHEMA public to 'aanedata_user';
+EOF
