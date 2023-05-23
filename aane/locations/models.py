4493d07e8754fae88de20e56b075c340f2d2d91d
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2023-05-23 18:35:53 -0400

    add Town table

--- aane/locations/models.py
+++ aane/locations/models.py
@@ -9,11 +9,13 @@ class State(models.Model):
     def __str__(self):
         return self.abbr
 
-# class Town(models.Model):
-#     # slug = models.SlugField('short name', max_length=32, unique=True)
-#     title = models.CharField(max_length=64)
-#     # note = models.TextField(blank=True, default='')   
+class Town(models.Model):
+    state = models.ForeignKey('State', on_delete=models.PROTECT,
+                              default=1)
+    # slug = models.SlugField('short name', max_length=32, unique=True)
+    title = models.CharField(max_length=64)
+    # note = models.TextField(blank=True, default='')   
 
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return self.title
 
