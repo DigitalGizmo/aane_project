294502aac34c06ddbd2c625dd0fe88258f1166eb
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2023-05-23 18:24:28 -0400

    add State admin

--- aane/locations/admin.py
+++ aane/locations/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import State
 
-# Register your models here.
+class StateAdmin(admin.ModelAdmin):
+    fields = [
+        'abbr'
+    ]
+    list_display = ('abbr',)
+
+
+admin.site.register(State, StateAdmin)
