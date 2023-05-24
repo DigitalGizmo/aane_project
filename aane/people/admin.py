7d8c5c088bfeb06941cf3fbf44cc9ab3a56f064c
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2023-05-24 12:35:49 -0400

    add person location

--- aane/people/admin.py
+++ aane/people/admin.py
@@ -5,24 +5,26 @@ from .models import AAPerson, OPerson
 class AAPersonAdmin(admin.ModelAdmin):
     fields = ['name', 'first_name','last_name', 'alt_name_spelling', 'freed_name', 'gender',
             'known_status', 'freed_status','birth_year', 
-            'death_year', 'bio', 'owners', 'place_of_origin', 
+            'death_year', 'bio', 'owners', 'locations', 'place_of_origin', 
             'owner_id','first_appearance_year', 'last_appearance_year', 
             'free_start_year']
     list_display = ('name', 'id', 'owner_id', 'first_name', 'last_name', 'alt_name_spelling', 
         'known_status', 'freed_status', 'free_start_year', 'birth_year', 'death_year')
     search_fields = ['name']
-    filter_horizontal = ['owners']
+    filter_horizontal = ['owners', 'locations']
     list_filter  = ['known_status', 'freed_status'] 
 
 admin.site.register(AAPerson, AAPersonAdmin)
 
 
 class OPersonAdmin(admin.ModelAdmin):
-    fields = ['name', 'first_name','last_name', 'title', 'gender', 'role', 'race', 
-            'bio', 'birth_year', 'death_year', 'year_lower', 
-            'year_upper']
+    fields = ['name', 'first_name','last_name', 'title', 
+              ('gender', 'role', 'race'), 
+            'bio', ('birth_year', 'death_year'), 
+            ('year_lower', 'year_upper'), 'locations']
     list_display = ('name', 'id', 'first_name', 'last_name', 'title', 'birth_year', 
             'death_year', 'year_lower')
+    filter_horizontal = ['locations']
     search_fields = ['name']
 
 admin.site.register(OPerson, OPersonAdmin)
