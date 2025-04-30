65abce44b0bd7dd83252454a91488641cc2cc9df
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2025-04-30 13:34:35 -0400

    back off from entry wysiwyg

--- aane/sources/admin.py
+++ aane/sources/admin.py
@@ -74,7 +74,7 @@ class SourceEntryAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': [
             'volume','primary_source', 
-            'entry_text', 'entry_text_html', 'interpretive_note',
+            'entry_text', 'interpretive_note', # 'entry_text_html', 
             'event',
             ('aa_id', 'transaction_note'),
             'aa_persons', 
